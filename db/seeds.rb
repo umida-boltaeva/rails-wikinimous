a3913ec5a5b8ab59c510c5a23ac3a8eb184295fd
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2023-02-20 16:43:54 +1100

    play with CRUD actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'faker'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# create 10 articles using faker gem
+10.times do
+  Article.create(title: Faker::Company.catch_phrase, content: Faker::Company.catch_phrase)
+end
